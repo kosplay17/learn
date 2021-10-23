0dc3efea28730f6696ebcb6655fdc61aa765ec4b
Pavel <kosplay17@gmail.com>
2021-10-23 15:43:32 +0300

    praktika 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-"use strict";
+
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false 
+}
+
+const a = prompt( "Один из последних просмотренных фильмов?". ""),
+      b = prompt("на сколько оцените его?", ""),
+      c = prompt( "Один из последних просмотренных фильмов?". ""),
+      d = prompt("на сколько оцените его?", "");
